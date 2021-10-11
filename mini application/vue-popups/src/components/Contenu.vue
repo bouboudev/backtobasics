441913bbb3e8d4a73267c831fd1799557efeacac
Bouzid Krita <bouzid.krita@hotmail.fr>
2021-10-11 18:25:14 +0200

    ajout du projet modale avec vue js et bootstrap mini app

---
+++ mini application/vue-popups/src/components/Contenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container my-5">
+    <h1> Modale avec Vue JS ! 😊</h1>
+    <h3 class="mb-5">(et un peu de Bootstrap...)</h3>
+    <h2  class="mb-5">↕</h2>
+
+    <modale :revele="revele" :toggleModale="toggleModale"/>
+    <div @click="toggleModale" class="btn btn-dark">Clique...</div>
+  </div>
+</template>
+
+
+<script>
+import Modale from "./Modale";
+
+export default {
+  name: "Contenu",
+  data() {
+    return {
+      revele: false
+    };
+  },
+  components: {
+    modale: Modale
+  },
+  methods: {
+    toggleModale: function() {
+      this.revele = !this.revele;
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
---
+++ mini application/vue-popups/src/components/Modale.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="bloc-modale" v-if="revele">
+    <div class="overlay" @click="toggleModale"></div>
+
+    <div class="modale card">
+      <div @click="toggleModale" class="btn-modale btn btn-danger">X</div>
+      <h2> Un peu de Lorem ipsum😎</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae tenetur
+        consequatur hic ipsam voluptas eveniet nulla rerum libero sed
+        dignissimos.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modale",
+  props: ["revele", "toggleModale"],
+};
+</script>
+
+<style scoped>
+.bloc-modale {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.overlay {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.modale {
+  background: #f1f1f1;
+  color: #333;
+  padding: 50px;
+  position: fixed;
+  top: 30%;
+}
+
+.btn-modale {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+</style>
